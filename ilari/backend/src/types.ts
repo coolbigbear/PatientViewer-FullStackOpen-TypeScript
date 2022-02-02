16f72bd7b3cce5559fd5e35a0018fdb2265807d1
coolbigbear <mrczuper@gmail.com>
2022-02-02 19:20:55 +0200

    Completed 9.19

--- ilari/backend/src/types.ts
+++ ilari/backend/src/types.ts
@@ -11,13 +11,6 @@ export interface DiagnoseEntry {
     latin?: string
 }
 
-export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
-export type NewPatientEntry = Omit<PatientEntry, 'id'>;
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export interface PatientEntry {
     id: string,
     name: string,
@@ -27,5 +20,49 @@ export interface PatientEntry {
     occupation: string,
     entries: Entry[]
 }
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
+export type NewPatientEntry = Omit<PatientEntry, 'id'>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+
+export interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<DiagnoseEntry['code']>;
+}
+
+export enum HealthCheckRating {
+	'Healthy' = 0,
+	'LowRisk' = 1,
+	'HighRisk' = 2,
+	'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: 'HealthCheck';
+	healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+	type: 'Hospital';
+    discharge: {
+        date: string,
+        criteria: string
+    }
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare',
+    employerName: string,
+	sickLeave?: {
+		startDate: string;
+        endDate: string;
+	};
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 
 export type PublicPatient = Omit<PatientEntry, 'ssn' | 'entries'>;
